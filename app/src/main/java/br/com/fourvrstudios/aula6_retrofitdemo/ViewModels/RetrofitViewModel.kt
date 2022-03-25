12741cd194e4cd5cc36d1f5e793592a187a15868
marcelookasaki <marcelo.okasaki@gmail.com>
2022-03-25 12:06:08 -0300

    Tratamento aprimorado de exceções

--- app/src/main/java/br/com/fourvrstudios/aula6_retrofitdemo/ViewModels/RetrofitViewModel.kt
+++ app/src/main/java/br/com/fourvrstudios/aula6_retrofitdemo/ViewModels/RetrofitViewModel.kt
@@ -11,11 +11,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
 import java.lang.Exception
 
+/* Classe para padronização de status */
+enum class RestApiStatus { LOADING, ERROR, DONE }
 
 class RetrofitViewModel : ViewModel() {
+
+    /* LiveData do tipo RestApiStatus para armazenar os status */
+    private val _status = MutableLiveData<RestApiStatus>()
+    val status: LiveData<RestApiStatus>
+        get() = _status
 
     private val _response = MutableLiveData<List<Photo>?>()
     val response: LiveData<List<Photo>?>
@@ -33,6 +42,7 @@ class RetrofitViewModel : ViewModel() {
         //postField()
     }
 
+    /* Forma Usando callbacks - Mais demorado
     private fun getRestApiResponse() {
         RetrofitInstance.retrofit.getAllCall().enqueue(object : Callback<List<Photo>>{
             override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
@@ -41,20 +51,31 @@ class RetrofitViewModel : ViewModel() {
                     _response.value = response.body()
                 }
             }
-
             override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
                 Log.i("RETROFIT_CALLL", "Falha: " + t.message)
             }
-
         })
-    }
+    } */
 
     fun getAllPhotos(): Job =
         viewModelScope.launch {
             try {
+                _status.value = RestApiStatus.LOADING
                 _response.value = RetrofitInstance.retrofit.getAll().body()
+                Log.i("MYTAG", "Success")
+                _status.value = RestApiStatus.DONE
+            } catch (e: IOException) {
+                Log.i("MYTAG", "IOException - Sem Internet, URL incorreta, etc")
+                _status.value = RestApiStatus.ERROR
+                _response.value = null
+            } catch (e: HttpException) {
+                Log.i("MYTAG", "HttpException - Status Codes não iniciados com 2XX")
+                _status.value = RestApiStatus.ERROR
+                _response.value = null
             } catch (e: Exception) {
                 Log.i("MYTAG", "Exceção Genérica")
+                _status.value = RestApiStatus.ERROR
+                _response.value = null
             }
         }
 
